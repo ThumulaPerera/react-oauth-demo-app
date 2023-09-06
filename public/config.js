f8687e7160dd980c79583dc4d5f99838b69e8e5c
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2023-09-06 12:40:22 +0530

    Add set background color from config

--- public/config.js
+++ public/config.js
@@ -1,5 +1,6 @@
 window.config = {
     testConfig: 'My custom config 2',
     itemsEndpoint: '/rbln/dummy-item-service/api-e04/1.0.0/items',
-    apiPrefix: '/api'
+    apiPrefix: '/api',
+    backgroundColor: 'rgba(218, 232, 252, 0.825)'
 };
