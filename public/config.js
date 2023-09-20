120652178b84747ce9c49dbafd02c70bba70ccf3
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2023-09-20 08:26:31 +0530

    Make app name and description configurble

--- public/config.js
+++ public/config.js
@@ -1,5 +1,6 @@
 window.config = {
-    testConfig: 'My custom config 2',
+    appName: 'App Name',
+    appDescription: 'App Description',
     itemsEndpoint: '/rbln/dummy-item-service/api-e04/1.0.0/items',
     apiPrefix: '/api',
     backgroundColor: 'rgba(218, 232, 252, 0.825)',
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,18 +1,19 @@
 import Typography from '@mui/joy/Typography';
 import Divider from '@mui/joy/Divider';
 
-const testConfig = window.config.testConfig;
+const appName = window.config.appName;
+const appDescription = window.config.appDescription;
 
 function LandingPage({ userInfo }) {
   let username = userInfo ? userInfo.email : "anonymous user";
 
   return (
     <div className="App">
-      <Typography level='h3'>React SPA</Typography>
+      <Typography level='h3'>{appName}</Typography>
+      <Typography level='body1'>{appDescription}</Typography>
       <Divider />
       <br />
       <Typography level='body1'>Welcome, {username} !</Typography>
-      <Typography level='body1'>Test config value: {testConfig}</Typography>
     </div>
   );
 }
